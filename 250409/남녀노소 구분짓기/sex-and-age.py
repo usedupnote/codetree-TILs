276d2f49a0dd24026e3e3f3eb673a7f7a18eb215
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2025-04-09 15:19:12 +0900

    Title: 남녀노소 구분짓기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250409/남녀노소 구분짓기/sex-and-age.py
@@ -0,0 +1,14 @@
+s = int(input())
+age = int(input())
+
+if age >= 19:
+    if s == 0:
+        print("MAN")
+    else:
+        print("WOMAN")
+
+else :
+    if s == 0:
+        print("BOY")
+    else:
+        print("GIRL")
