086502c17e8a6e7383b5bbabbd6badba2ab8e86e
Lee Jeongmyeong [이정명] <59154924+usedupnote@users.noreply.github.com>
2025-04-11 21:16:25 +0900

    Title: 약수가 세 개인 수, Time: 53ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250411/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -0,0 +1,14 @@
+start, end = map(int, input().split())
+
+result = 0
+
+for i in range(start, end+1) :
+    cnt = 2
+    for j in range(2, i):
+        if i % j == 0:
+            cnt+=1
+        if cnt > 3:
+            break
+    if cnt == 3:
+        result+=1
+print(result)
